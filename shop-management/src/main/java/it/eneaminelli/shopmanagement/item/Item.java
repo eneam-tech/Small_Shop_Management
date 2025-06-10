0bd8ad68773e8af76ef71b3838ee208fe6976ef3
eneam-tech <eneaminelli+github@gmail.com>
2025-06-10 12:05:08 +0200

    Added builder pattern in Item for more personalized item creation

--- shop-management/src/main/java/it/eneaminelli/shopmanagement/item/Item.java
+++ shop-management/src/main/java/it/eneaminelli/shopmanagement/item/Item.java
@@ -1,20 +1,21 @@
 package it.eneaminelli.shopmanagement.item;
 
 public class Item implements ItemInterface {
-    private String name;
-    private double price;
-    private String productID;
+    private final String name;
+    private final double price;
+    private final String productID;
+    private final ItemType itemType;
     private int stock;
-    private ItemType itemType;
 
-    public Item(String name, double price, String productID, int stock, ItemType itemType) {
-        this.name = name;
-        this.price = price;
-        this.productID = productID;
-        this.stock = stock;
-        this.itemType = itemType;
+    private Item(ItemBuilder builder) {
+        this.name = builder.name;
+        this.price = builder.price;
+        this.productID = builder.productID;
+        this.stock = builder.stock;
+        this.itemType = builder.itemType;
     }
 
+    // -----------------------> Getters and setters <----------------------------
     @Override
     public String toString(){
         return "Item characteristics:\n - Name: " + name + "\n - Price: " + price + "\n - Stock: " + stock + "\n - Serial: " + productID + "\n - Item type: " + itemType;
@@ -40,6 +41,47 @@ public class Item implements ItemInterface {
 
     public String prepareForSaving(){
         return name + productID + itemType.toString(); 
+    }
+
+    // -------------------> End of Getters and setters <------------------------
+
+    //Builder pattern for personalized item creation
+    public static class ItemBuilder{
+        private String name;
+        private double price;
+        private String productID;
+        private int stock;
+        private ItemType itemType;
+        
+        public ItemBuilder(String productID, String name) {
+            this.productID = productID;
+            this.name = name;
+        }
+
+        public ItemBuilder withPrice(double price){
+            this.price = price;
+            return this;
+        }
+
+        public ItemBuilder withStock(int stock){
+            this.stock = stock;
+            return this;
+        }
+
+        public ItemBuilder ofType(ItemType itemType){
+            this.itemType = itemType;
+            return this;
+        }
+
+        //Builde method for immutable object
+        public Item build(){
+            if(name == null || productID == null) {
+                throw new IllegalStateException("Name and ProductID cannot be null.");
+            }
+
+            return new Item(this);
+        }
+        
     }
 
 }
