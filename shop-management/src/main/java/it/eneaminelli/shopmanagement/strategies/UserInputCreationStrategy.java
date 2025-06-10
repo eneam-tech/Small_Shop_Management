8747282b6f227cc75f541053e56fb982e0183041
eneam-tech <eneaminelli+github@gmail.com>
2025-06-10 13:14:04 +0200

    Added user input creation strategy

--- shop-management/src/main/java/it/eneaminelli/shopmanagement/strategies/UserInputCreationStrategy.java
+++ shop-management/src/main/java/it/eneaminelli/shopmanagement/strategies/UserInputCreationStrategy.java
@@ -1,5 +1,23 @@
 package it.eneaminelli.shopmanagement.strategies;
 
-public class UserInputCreationStrategy {
-    
+import java.util.Scanner;
+
+import it.eneaminelli.shopmanagement.item.Item;
+
+public class UserInputCreationStrategy implements ItemCreationStrategy {
+    @Override
+    public Item create(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter product ID: ");
+        String id = scanner.nextLine();
+        System.out.println("Enter name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter price: ");
+        double price = scanner.nextDouble();
+        System.out.println("Enter stock: ");
+        int stock = scanner.nextInt();
+        //TODO: add itemtype/ofType selection
+
+        return new Item.ItemBuilder(id, name).withPrice(price).withStock(stock).build();
+    }
 }
