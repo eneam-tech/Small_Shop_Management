6d24b56e25a1cd5fe1b2b78551bf144bdd8704de
eneam-tech <eneaminelli+github@gmail.com>
2025-09-05 11:37:54 +0200

    Minimal changes for smoother setup

--- shop-management/src/main/java/it/eneaminelli/shopmanagement/Application.java
+++ shop-management/src/main/java/it/eneaminelli/shopmanagement/Application.java
@@ -30,7 +30,7 @@ public class Application {
 
     public static void main(String[] args) {
         Application app = new Application();
-        app.run();
+        app.run(); 
     }
 
     public void run(){
